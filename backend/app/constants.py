24957f1d30fb25873d8cf345c535897bcf2c7c11
Pushp Vashisht <pushptyagi1@gmail.com>
2024-01-21 18:03:27 +0000

    Add constants for OpenWeather API

---
+++ backend/app/constants.py
@@ -0,0 +1,6 @@
+from os import getenv
+
+OPENWEATHER_API_KEY = getenv('OPENWEATHER_API_KEY')
+OPENWEATHER_RESPONSE_LIMIT = 5
+OPENWEATHER_GEO_CODE_URL = 'https://api.openweathermap.org/geo/1.0/direct'
+OPENWEATHER_CURRENT_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather'
